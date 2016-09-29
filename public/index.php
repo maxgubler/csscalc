de77f686d50ea86289d48cc445dd25bd5ba62e59
Max Gubler <maxwell.j.gubler@gmail.com>
2016-09-29 14:59:41 +0000

    Add F buttons to calc. Center with flexbox

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" rel="stylesheet">
+        <link href="/css/calc.css" rel="stylesheet">
+        
+        <title>MX-89 Titanium</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div id="col-half">
+                    <div id="calc-outer">
+                        <div id="inner-wrap">
+                            <div id="calc-inner">
+                            </div>
+                            <div id="calc-screen">
+                                <div id="calc-screen-shadow-top">
+                                </div>
+                            </div>
+                            <div id="calc-screen-spacer">
+                            </div>
+                            <div id="fbutton-group">
+                                <div class="fbutton"><span>F1</span></div>
+                                <div class="fbutton"><span>F2</span></div>
+                                <div class="fbutton"><span>F3</span></div>
+                                <div class="fbutton"><span>F4</span></div>
+                                <div class="fbutton"><span>F5</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
