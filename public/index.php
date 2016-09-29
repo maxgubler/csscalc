e85293d014b3db1f0870b1a3d203e6dcd28c33f8
Max Gubler <maxwell.j.gubler@gmail.com>
2016-09-29 16:53:03 +0000

    Align 2nd column

--- public/index.php
+++ public/index.php
@@ -30,6 +30,30 @@
                                 <div class="fbutton"><span>F4</span></div>
                                 <div class="fbutton"><span>F5</span></div>
                             </div>
+                            <div id="columns">
+                                <div id="col-1">
+                                    <div id="col-1-second" class="col-1-btn">2ND</div>
+                                    <div id="col-1-diamond" class="col-1-btn"><></div>
+                                    <div id="col-1-home" class="col-1-btn">HOME</div>
+                                    <div id="col-1-x" class="col-1-btn">X</div>
+                                    <div id="col-1-equal" class="col-1-btn">=</div>
+                                    <div id="col-1-pipe" class="col-1-btn">|</div>
+                                    <div id="col-1-ee" class="col-1-btn">EE</div>
+                                    <div id="col-1-store" class="col-1-btn">STO></div>
+                                    <div id="col-1-on" class="col-1-btn">ON</div>
+                                </div>
+                                <div id="col-2">
+                                    <div id="col-2-shift" class="col-2-btn">^</div>
+                                    <div id="col-2-alpha" class="col-2-btn">ALPHA</div>
+                                    <div id="col-2-mode" class="col-2-btn">MODE</div>
+                                    <div id="col-2-y" class="col-2-btn">Y</div>
+                                    <div id="col-2-leftpar" class="col-2-btn">(</div>
+                                    <div id="col-2-7" class="col-2-btn-blk">7</div>
+                                    <div id="col-2-4" class="col-2-btn-blk">4</div>
+                                    <div id="col-2-1" class="col-2-btn-blk">1</div>
+                                    <div id="col-2-0" class="col-2-btn-blk">0</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
