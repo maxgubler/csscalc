bdfcc5fa5e52c7299f73e1c795a9522a6d7e7111
Max Gubler <maxwell.j.gubler@gmail.com>
2017-03-31 03:07:41 +0000

    Fix css shape / background-radius scaling issue

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,5 @@ $(function(){
     $(window).resize(function() {
         var h = $('#calc-outer').height();
         $('#calc-outer').width( h / 2);
-        
-        var w = $('#calc-outer').width();
-        var brString = h/10 + '% ' + h/10 + '% ' + w/10 + '% ' + w/10 + '% / 4% 4% 17% 17%';
-        $('#calc-outer').css({'border-radius': brString});
-        /*$('calc-outer').attr('style', 'border-radius: 53.75% 4% 53.75% 4% / 26.875% 17% 26.875% 50%;');*/
     });
 });
