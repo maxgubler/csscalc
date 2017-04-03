97dafe92adb93a43741f74e458213800d38142b1
Max Gubler <maxwell.j.gubler@gmail.com>
2017-04-03 23:32:07 +0000

    Evaluate input string on click of enter button

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,16 @@
 $(function(){
+    // RESIZE
     $('#calc-outer').css('width', $('#calc-outer').height() / 2);
     $(window).resize(function() {
         var h = $('#calc-outer').height();
         $('#calc-outer').width( h / 2);
+    });
+    
+    // MATH
+    $('#col-5-enter').click(function(){
+        $('#calc-screen-text-entry').val(function(index, entry){
+           var result = math.eval(entry);
+           return result;
+        });
     });
 });
