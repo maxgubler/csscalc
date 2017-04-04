dfc3f97e3818ba2bfc79b2204f1618d29cfe1e07
Max Gubler <maxwell.j.gubler@gmail.com>
2017-04-04 03:13:54 +0000

    Append numbers and symbols to input on button click. Fix comma button

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-    var evaluate, entryFocus;
+    var evaluate, entryFocus, type;
     
     // RESIZE
     $('#calc-outer').css('width', $('#calc-outer').height() / 2);
@@ -21,6 +21,15 @@ $(function(){
     entryFocus = function() {
         $('#calc-screen-text-entry').focus();
     };
+    
+    type = function(val, id) {
+        $(id).click(function(){
+            var entry = $('#calc-screen-text-entry').val();
+            entry += val;
+            $('#calc-screen-text-entry').val(entry);
+            entryFocus();
+        });
+    };
 
 
     // BUTTONS
@@ -28,11 +37,35 @@ $(function(){
     $('#col-5-enter').click(function(){
         evaluate();
     });
+    
     // Clear
     $('#col-5-clear').click(function(){
         $('#calc-screen-text-entry').val('');
         entryFocus();
     });
+    
+    // Numbers
+    type('0', '#col-2-0');
+    type('1', '#col-2-1');
+    type('2', '#col-3-2');
+    type('3', '#col-4-3');
+    type('4', '#col-2-4');
+    type('5', '#col-3-5');
+    type('6', '#col-4-6');
+    type('7', '#col-2-7');
+    type('8', '#col-3-8');
+    type('9', '#col-4-9');
+    
+    // Symbols
+    type('=', '#col-1-equal');
+    type('(', '#col-2-leftpar');
+    type(')', '#col-3-rightpar');
+    type(',', '#col-4-comma');
+    type('/', '#col-5-div');
+    type('*', '#col-5-mult');
+    type('-', '#col-5-minus');
+    type('+', '#col-5-plus');
+    type('.', '#col-3-period');
     
     // KEYS
     // Enter / Return
