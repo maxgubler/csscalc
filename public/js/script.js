eecdc55b5fbd23ed6ffe57a4d6f911b393b311a8
Max Gubler <maxwell.j.gubler@gmail.com>
2017-04-04 00:14:00 +0000

    Enable Clear button and Enter key. Create functions for evaluate and entryFocus

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 $(function(){
+    var evaluate, entryFocus;
+    
     // RESIZE
     $('#calc-outer').css('width', $('#calc-outer').height() / 2);
     $(window).resize(function() {
@@ -7,10 +9,37 @@ $(function(){
     });
     
     // MATH
-    $('#col-5-enter').click(function(){
+    evaluate = function() {
         $('#calc-screen-text-entry').val(function(index, entry){
            var result = math.eval(entry);
            return result;
         });
+        entryFocus();
+    };
+    
+    // MISC
+    entryFocus = function() {
+        $('#calc-screen-text-entry').focus();
+    };
+
+
+    // BUTTONS
+    // Enter
+    $('#col-5-enter').click(function(){
+        evaluate();
+    });
+    // Clear
+    $('#col-5-clear').click(function(){
+        $('#calc-screen-text-entry').val('');
+        entryFocus();
+    });
+    
+    // KEYS
+    // Enter / Return
+    $(window).keypress(function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            evaluate();
+        }
     });
 });
